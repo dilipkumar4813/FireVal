1b2ea14a79946c9d32c0dc06aaeebb21a6c7b0b3
dilip <dilipkumar4813@gmail.com>
2018-11-07 14:31:33 +0800

    Initial Commit

---
+++ FireVal/FireVal/BasicValidations.swift
@@ -0,0 +1,22 @@
+//
+//  BasicValidations.swift
+//  FireVal
+//
+//  Created by Dilip kumar on 07/11/2018.
+//  Copyright © 2018 Dilip kumar. All rights reserved.
+//
+
+import Foundation
+
+public class BasicValidations {
+	
+	func isValidUsername(username: String,minCharacters: Int, maxCharacters: Int) -> Bool {
+		let RegEx = "\\w{\(minCharacters),\(maxCharacters)}"
+		let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
+		return Test.evaluate(with: username)
+	}
+	
+	func isValidPassword(password: String) {
+		
+	}
+}
---
+++ FireVal/FireVal/FireVal.h
@@ -0,0 +1,19 @@
+//
+//  FireVal.h
+//  FireVal
+//
+//  Created by Dilip kumar on 07/11/2018.
+//  Copyright © 2018 Dilip kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FireVal.
+FOUNDATION_EXPORT double FireValVersionNumber;
+
+//! Project version string for FireVal.
+FOUNDATION_EXPORT const unsigned char FireValVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FireVal/PublicHeader.h>
+
+
